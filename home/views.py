48b4ff891fc9fed733cff8b06327b477eee1e1ce
Shimul <shimul.official7@gmail.com>
2020-10-14 20:04:39 +0600

    Unlike Button Added

--- home/views.py
+++ home/views.py
@@ -8,7 +8,15 @@ from django.contrib import messages
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article_details', args=[str(pk)]))
     
 
@@ -34,10 +42,15 @@ class ArticleDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
-        context["total_likes"] = total_likes 
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context["total_likes"] = total_likes
+        context["liked"] = liked 
         return context
     
     
